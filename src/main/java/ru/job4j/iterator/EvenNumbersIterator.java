9431be8314fc137d633b02a73c7ab7686f4d3e3d
Denis Lazykin <dlazykin856@gmail.com>
2023-02-02 19:30:42 +0300

    5.1.2.Итератор четных чисел [#150].

---
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,21 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() { }
+
+    @Override
+    public Integer next() { }
+
+}
