8c226934883ba871e42cc6d47cc3c73c70310d76
Denis Lazykin <dlazykin856@gmail.com>
2023-03-05 09:12:00 +0300

    0. Что такое обобщенные типы (generics) [#4952 #389658].Рефакторинг класса Person.

--- src/main/java/ru/job4j/generics/Person.java
+++ src/main/java/ru/job4j/generics/Person.java
@@ -5,9 +5,7 @@ import java.util.Objects;
 
 public class Person {
     private String name;
-
     private int age;
-
     private Date birthday;
 
     public Person(String name, int age, Date birthday) {
